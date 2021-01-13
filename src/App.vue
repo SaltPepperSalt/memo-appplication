f7bdd290c86eacf9c4f290771112b39ea4850008
SaltPepperSalt <dksro324@gmail.com>
2021-01-13 22:52:09 +0900

    feat: create andd read function
    create and read memo on localstorage

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <app-header />
+    <memo-app />
+  </div>
+</template>
+
+<script>
+/* eslint-disable */
+import AppHeader from "./components/AppHeader.vue";
+import MemoApp from "./components/MemoApp.vue";
+
+export default {
+  name: "App",
+  components: {
+    AppHeader,
+    MemoApp,
+  },
+};
+</script>
+
+<style>
+@import "styles/reset.css";
+#app {
+  width: 560px;
+  margin: 0 auto;
+}
+</style>
